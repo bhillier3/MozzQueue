5002529679bf0f4c5d015a994d15705106048960
bhillier3 <bhillier3@gatech.edu>
2022-08-31 21:30:03 -0500

    fix bug where titles/error msgs weren't updating properly on DOM

--- src/components/MozzForm.js
+++ src/components/MozzForm.js
@@ -5,24 +5,19 @@ import { addDoc, collection, Timestamp } from 'firebase/firestore'
 const db = firebase.firestore()
 
 /*
-  Idea: Popout initially
-  Survey style where one thing pops out to select each time
-  1. Server (from active servers)
-  2. Table # (user inputs)
-  3. Notes (maybe button for preselecting bday and anniversary?)
+  Notes (maybe button for preselecting bday and anniversary?)
 */
 
 const MozzForm = ({ togglePop, servers }) => {
   const [selectedServer, setSelectedServer] = useState('')
-  // const [errMsg, setErrMsg] = useState('')
   const tableRef = useRef('')
   const notesRef = useRef('')
-  const servTitleRef = useRef('')
-  const tableTitleRef = useRef('')
+  const servTitleRef = useRef()
+  const tableTitleRef = useRef()
 
   useEffect(() => {
-    servTitleRef.current.value = 'Select Server'
-    tableTitleRef.current.value = 'Table #'
+    servTitleRef.current.textContent = 'Select Server'
+    tableTitleRef.current.textContent = 'Table #'
   }, [])
 
 
@@ -32,20 +27,20 @@ const MozzForm = ({ togglePop, servers }) => {
 
   const handleSumbit = async(e) => {
     e.preventDefault()
-    
-    servTitleRef.current.value = 'Select Server'
+
+    servTitleRef.current.textContent = 'Select Server'
     servTitleRef.current.style.color = '#5c7e87'
-    tableTitleRef.current.value = 'Table #'
+    tableTitleRef.current.textContent = 'Table #'
     tableTitleRef.current.style.color = '#5c7e87'
 
     // ERROR CHECKS
     if (selectedServer == '' || tableRef.current.value == '') {
       if (selectedServer == '') {
-        servTitleRef.current.value = 'Must Select A Server'
+        servTitleRef.current.textContent = 'Must Select A Server'
         servTitleRef.current.style.color = '#89111a'
       }
       if (tableRef.current.value == '') {
-        tableTitleRef.current.value = 'Must Input A Table #'
+        tableTitleRef.current.textContent = 'Must Input A Table #'
         tableTitleRef.current.style.color = '#89111a'
         tableTitleRef.current.style.textAlign = 'center'
       }
@@ -75,9 +70,7 @@ const MozzForm = ({ togglePop, servers }) => {
         <h3 className='form-title'>Mozzarella Sign Up</h3>
         <div className='form-grid'>
           <div className='form-servers'>
-            <h4 className='form-heading' ref={servTitleRef}>
-              {servTitleRef.current.value}
-            </h4>
+            <h4 className='form-heading' ref={servTitleRef}></h4>
             <div className='form-servers-box'>
             {servers.map(server =>
               <FormServerSelectBtn
@@ -90,7 +83,7 @@ const MozzForm = ({ togglePop, servers }) => {
             </div>
           </div>
           <div className='form-tablenum'>
-            <h4 ref={tableTitleRef}>{tableTitleRef.current.value}</h4>
+            <h4 ref={tableTitleRef}></h4>
             <input className='table' type="number" ref={tableRef} />
           </div>
           <div className='form-notes'>
